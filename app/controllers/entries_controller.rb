6d18f9c1e60d322f48ebf35695969388d1458c6a
Fang <fang.zhang@kellogg.northwestern.edu>
2026-03-11 05:32:01 +0000

    changes 3

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,23 +2,24 @@ class EntriesController < ApplicationController
 
   def new
     if @current_user == nil
-    redirect_to "/login"
+      redirect_to "/login"
+    end
   end
 
   def create
-   if @current_user != nil
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = @current_user["id"]
-    @entry.uploaded_image.attach(params["uploaded_image"])
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
-   else
+    if @current_user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
       redirect_to "/login"
-   end
+    end
   end
 
 end
