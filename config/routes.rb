e339e60bab23e610413b35c8f41e3acd193bf1d8
Fang <fang.zhang@kellogg.northwestern.edu>
2026-03-11 14:07:36 +0000

    changes 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   post("/users", { :controller => "users", :action => "create" })
   
   get("/login", { :controller => "sessions", :action => "new" })
-  post("/sessions", { :controller => "sessions", :action => "create" })
+  post("/login", { :controller => "sessions", :action => "create" })
   get("/logout", { :controller => "sessions", :action => "destroy" })
   
   resources "entries"
   resources "places"
 end
+
